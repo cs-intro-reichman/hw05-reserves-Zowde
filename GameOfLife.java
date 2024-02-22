b79a2b7401307a4b272d940ed94be3924db38f23
Zowde <157274222+Zowde@users.noreply.github.com>
2024-02-22 09:05:30 +0200

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -182,6 +182,9 @@ public class GameOfLife {
 		{
 			for(int j=1;j<arr[i].length-1;j++)
 			{
+				if(j==arr[i].length-2)
+					System.out.print("  "+arr[i][j]);
+				else
 				System.out.print(arr[i][j]+"  ");
 			}
 			System.out.println();
